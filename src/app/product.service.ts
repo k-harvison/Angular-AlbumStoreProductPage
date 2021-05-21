de8b69ea25a854ebe3c57f9730572e699f8389a7
k-harvison <68281791+k-harvison@users.noreply.github.com>
2021-05-21 15:16:49 -0400

    module 3 mid

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { http, response } from @angular/http;
+
+@Injectable()
+export class ProductService {
+
+  constructor() { private _http: Http}
+
+}
