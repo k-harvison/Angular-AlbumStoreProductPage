033f40a8e4d3ea94710258917cb8823aa4ac382f
k-harvison <68281791+k-harvison@users.noreply.github.com>
2021-05-25 08:19:16 -0400

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/add/oeprator/map';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
