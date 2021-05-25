93a4183768564c8c0556427bed9f2a5967e5e74d
k-harvison <68281791+k-harvison@users.noreply.github.com>
2021-05-25 11:12:12 -0400

    update

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,10 +1,10 @@
-import { Track } from './/track'
+import { Track } from './track'
 
 export interface Album {
     name: string;
     releaseDate: string;
     coverImage: string;
-    tracks: Track [];
+    tracks: Track[];
 
 }
 
